ad003e6f14fe678fce891baec2c94bc1faa59d97
Noxer <tim@plusmid.net>
2013-01-25 12:14:29 +0100

    sh started

---
+++ src/apps/sh/sh.c
@@ -0,0 +1,15 @@
+
+#include <atlas2.h>
+#include <stdio.h>
+
+#define TITLE "Atlas2 SHell"
+#define VERSION_MAIN 0
+#define VERSION_SUB 1
+
+int main(int argc, char *argv[])
+{
+
+	printf("%s v%u.%u", TITLE, VERSION_MAIN, VERSION_SUB);
+
+}
+
